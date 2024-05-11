75726ec687419bd8c5b6b0754e97651a2d529c1d
Mr-Ples <simonsingularitygroup@gmail.com>
2024-05-11 21:41:39 +0200

    update: use smart pointer + new Keyboard interface

--- main.cpp
+++ main.cpp
@@ -1,42 +1,38 @@
-#define WLR_USE_UNSTABLE
-
 #include <unordered_map>
 #include <format>
 
 #include <hyprland/src/managers/EventManager.hpp>
 #include <hyprland/src/managers/input/InputManager.hpp>
 #include <hyprland/src/plugins/PluginAPI.hpp>
+#include <hyprland/src/devices/IKeyboard.hpp>
 
 #include "globals.hpp"
 
 void onKeypress(void *self, std::any data) {
   auto map = std::any_cast<std::unordered_map<std::string, std::any>>(data);
-  std::any k = map["keyboard"];
-  wlr_keyboard_key_event *e =
-      std::any_cast<wlr_keyboard_key_event *>(map["event"]);
-  SKeyboard *keyboard = std::any_cast<SKeyboard *>(k);
+
+  auto keyboard = std::any_cast<SP<IKeyboard>>(map["keyboard"]);
+  auto e = std::any_cast<IKeyboard::SKeyEvent>( map["event"]);
 
   const auto MODS = g_pInputManager->accumulateModsFromAllKBs();
-  const auto STATE = e->state == WL_KEYBOARD_KEY_STATE_PRESSED ? "down" : "up";
+  const auto STATE = e.state == WL_KEYBOARD_KEY_STATE_PRESSED ? "down" : "up";
   const auto KEY = "";
-  const auto KEYCODE = e->keycode + 8;
+  const auto KEYCODE = e.keycode + 8;
 
   g_pEventManager->postEvent(SHyprIPCEvent{
-          "keypress", std::format("{},{},{},{},{}", keyboard->name , MODS, STATE, KEY, KEYCODE)
+          "keypress", std::format("{},{},{},{},{}", keyboard->hlName, MODS, STATE, KEY, KEYCODE)
   });
 
-  if (keyboard->name == "by-tech-usb-gaming-keyboard") {
+  if (keyboard->hlName == "by-tech-usb-gaming-keyboard") {
       /* system("notify-send hi"); */
   }
 }
 
 APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
   PHANDLE = handle;
-  if (!HyprlandAPI::registerCallbackDynamic(
-          PHANDLE, "keyPress",
-          [&](void *self, SCallbackInfo &info, std::any data) {
-            onKeypress(self, data);
-          })) {
+
+  static auto P  = HyprlandAPI::registerCallbackDynamic(PHANDLE, "keyPress", [&](void* self, SCallbackInfo& info, std::any data) { onKeypress(self, data); });
+  if (!P) {
     HyprlandAPI::addNotification(PHANDLE, "Failed to add handler",
                                  CColor{0.0, 1.0, 1.0, 1.0}, 5000);
   }
